c40d7c135051b7178f2a5dd956d2512014321b31
Andrew Liu <andrewliu33@gmail.com>
2011-07-18 21:04:07 +0800

    Going to release!

--- lib/killsite.rb
+++ lib/killsite.rb
@@ -1,48 +1,41 @@
-require 'uri'
-require 'em-http-request'
+require 'open-uri'
 require 'nokogiri'
 
 class SiteKiller
-  def initialize(prefix, limit = 1, verbose = false, observers = [])
-    @count = 1
+  def initialize(prefix, limit = nil, concurrency = nil, verbose = nil, observers = nil)
     @prefix = URI.parse(prefix)
     @visited = [@prefix]
-    @verbose = verbose
-    @limit = limit
-    @observers = observers
+    @queue = [@prefix]
+    @verbose = verbose || false
+    @limit = limit || 1
+    @concurrency = concurrency || 10
+    @concurrency = @limit if @concurrency > @limit
+    @observers = observers || []
   end
 
-  def run url = @prefix
-    @observers.each { |o| o.before_request(url) if o.respond_to? :before_request }
+  def run
+    while @queue.size > 0
+      url = @queue.shift
     
-    multi = EventMachine::MultiRequest.new
-    @limit.times { multi.add(EventMachine::HttpRequest.new(url).get) }
-    
-    multi.callback do
+      @observers.each { |o| o.before_request(url) if o.respond_to? :before_request }
+      `ab -n #{@limit} -c #{@concurrency} #{url}`
       @observers.each { |o| o.after_request(url) if o.respond_to? :after_request }
       
-      http = (multi.responses[:succeeded].size > 0) ? multi.responses[:succeeded].first : nil
-      
       puts "Processing '#{url}'" if @verbose
-      if http
-        Nokogiri::HTML.parse(http.response).xpath("//a[@href]").each do |link|
+      begin
+        response = open(url).read
+        Nokogiri::HTML.parse(response).xpath("//a[@href]").each do |link|
           next_url = process_url link['href']
           if next_url and !@visited.include?(next_url)
             puts "  Queueing '#{next_url}'" if @verbose
 
             @visited << next_url
-            @count += 1
-            run next_url
+            @queue << next_url
           end
         end
-      else
-        puts "  No valid response"
+      rescue => e
+        puts "  No valid response: #{e}"
       end
-      puts "  Progress #{'*' * multi.responses[:succeeded].size}#{'X' * multi.responses[:failed].size}" if @verbose
-      $stdout.flush
-      
-      @count -= 1
-      EventMachine.stop if @count == 0
     end
   end
 
